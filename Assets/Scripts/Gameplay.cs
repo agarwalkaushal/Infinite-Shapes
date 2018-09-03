575feaf631a1576bf795c827b01c3707c73059b7
kaushalpaulite <kaushalagarwal79@gmail.com>
2018-09-03 13:50:59 +0530

    Project created

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gameplay : MonoBehaviour {
+
+    public static bool startGame = false;
+    public static bool gameOver = false;
+
+    private int randomPrefabIndex;
+    private int currentShape=0;
+
+    private float timeSinceLastSpawned;
+    private float spawnRate = 2f;
+    private float spawnXPosition;
+    private float spawnYPosition;
+
+    private Vector2 objectPoolPosition = new Vector2(-5f, 0f);
+
+    public GameObject player;
+    public GameObject tap;
+    public GameObject oval;
+    public GameObject triangle;
+    public GameObject square;
+
+    private GameObject[] gameObjects;
+    private GameObject[] randomGameObjects;
+
+    // Use this for initialization
+    void Start () {
+
+        gameObjects = new GameObject[3];
+        randomGameObjects = new GameObject[6];
+        gameObjects[0] = oval;
+        gameObjects[1] = triangle;
+        gameObjects[2] = square;
+
+        SpawnRandom();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if (startGame)
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * 2.5f);
+            if(timeSinceLastSpawned >= spawnRate)
+            {
+                timeSinceLastSpawned = 0;
+                spawnXPosition = Random.Range(-2f, 2f);
+                spawnYPosition = player.transform.position.y + 5f;
+                randomGameObjects[currentShape].transform.Rotate(Vector3.back, Time.deltaTime);
+                randomGameObjects[currentShape++].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+                
+                if(currentShape>5)
+                {
+                    SpawnRandom();
+                    currentShape = 0;
+                }
+
+            }
+        }
+
+		
+	}
+
+    void SpawnRandom()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            randomPrefabIndex = Random.Range(0, 3);
+            randomGameObjects[i] = Instantiate(gameObjects[randomPrefabIndex], objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+    public void tapToBegin()
+    {
+        startGame = true;
+        tap.SetActive(false);
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 2.5f; //speed of the player,camera
+    private int c = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        // initially, the temporary vector should equal the player's position
+        Vector2 clampedPosition = transform.position;
+        // Now we can manipulte it to clamp the y element
+        clampedPosition.x = Mathf.Clamp(transform.position.x, -2f,2f);
+        // re-assigning the transform's position will clamp it
+        transform.position = clampedPosition;
+
+        if (Gameplay.startGame)
+        {
+            //Keep the player moving in the upward direction
+            transform.Translate(Vector2.up * Time.deltaTime * speed);
+
+            //Checks for the keypress A, if true then translate in left direction
+            if (Input.GetKey(KeyCode.A))
+                transform.Translate(Vector2.left * Time.deltaTime * 9);
+
+            //Checks for the keypress D, if true then translate in right direction
+            if (Input.GetKey(KeyCode.D))
+                transform.Translate(Vector2.right * Time.deltaTime * 9);
+
+            transform.Translate(Input.acceleration.x * Time.deltaTime * 9,0,0);
+        }
+
+    }
+}
